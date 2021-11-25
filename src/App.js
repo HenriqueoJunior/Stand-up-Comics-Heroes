b8a4791fc3fc58a80eb729850efed1f6647f9df5
LiarKiller <antoniohenriquemoura@hotmail.com>
2021-11-25 02:45:53 -0300

    Initial code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./paginas/Home";
+import "./App.css"
+
+
+function App() {
+  return (
+    <div>
+    <Home></Home>
+    
+  </div>
+  );
+}
+
+export default App;
---
+++ src/paginas/Home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Cabecalho from "./componentes/Cabecalho";
+
+function Home() {
+    return (
+    <div>
+        <Cabecalho />
+        <main >
+            <div >
+                Navbar
+            </div>
+            <div >
+                Feed
+            </div>
+            
+        </main>
+    </div>
+    )
+}
+
+export default Home
